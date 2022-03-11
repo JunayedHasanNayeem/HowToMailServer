42e5be917724a975885021a0dd982bf772ceb555
Junayed Hasan <junayedhasannayeem@gmail.com>
2022-03-11 23:02:39 +0600

    Version 0.01

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId
 const cors = require('cors');
 require("dotenv").config()
 const request = require('request');
@@ -9,11 +11,20 @@ const port = process.env.PORT || 5000;
 app.use(express.json())
 app.use(cors())
 
+//MongoDB Connect
+const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.fpc6f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 async function run() {
     try {
+        //Mongo Client
+        client.connect()
+        const database = client.db('HowToMail')
+        const usersCollection = database.collection('users');
+
 
         //Post SearchInfo - API
-        app.post('/search-result', async(req, res) => {
+        app.post('/search-result', async (req, res) => {
             const searchInfo = req.body;
             request({
                 method: 'GET',
@@ -30,15 +41,55 @@ async function run() {
         })
 
         //GET SearchResults - API 
-        app.get('/results', async (req, res) => {
-           
+        app.get('/users', async (req, res) => {
+            const cursor = usersCollection.find({});
+            const users = await cursor.toArray();
+            res.send(users)
+
         });
 
-        //Post SearchInfo API
+        // GET USER 
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await usersCollection.findOne({ email: email });
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true
+            }
+            res.json({ admin: isAdmin })
+        })
 
+        //POST USER - API
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user)
+            console.log(result)
+        })
 
+        //PUT USER - API
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options)
+        })
 
-
+        //PUT CREDITS - API
+        app.put('/users/add-credits', async(req, res) => {
+            const credits = req.body;
+            const filter = {email: credits.email}
+            const user = await usersCollection.findOne({ email: credits.email })
+            let currentCredits;
+            if(user?.credits){
+                currentCredits = parseFloat(user?.credits);
+            } else {
+                currentCredits = 0
+            }
+            const updateDoc = await {$set:{credits: parseFloat(credits?.credits) + currentCredits}}
+            const result = await usersCollection.updateOne(filter, updateDoc)
+            res.send({currentCredits})
+        })
 
     }
     finally {
